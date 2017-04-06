b6a4562a0a3dabe64c680eeb8637578f81e0540a
xinon <xinon@ZJB.local>
2017-04-06 17:02:38 +0800

    .

--- LoadScript.js
+++ LoadScript.js
@@ -7,42 +7,51 @@ var util = require('util');
 
 function ParseSqlParameters(Template, postData) {
     var r = {};
-    var ScriptType = Template['SqlScript'].$['type'];
-    var Command = Template['SqlScript']['Command'];
-    var Parameters = Template['SqlScript']['Parameters'];
-    var ResultType = Template['SqlScript']['ResultType'];
-    var sql = Command[0].toString();
-    var selectSql = '';//select @returnValue, @returnMsg;
+    try {
 
-    if (Parameters.length == 1) {
-        for (var i = 0; i < Parameters[0].par.length; i++) {
-            //https://www.npmjs.com/package/xml2js
-            //attrkey (default: $): Prefix that is used to access the attributes. Version 0.1 default was @.
-            var par = Parameters[0].par[i].$;
-            var par_name = par.name;
-            var par_type = par.type;
-            var par_dir = par.dir;
-            var par_value = postData[par_name.replace('@', '')];
+        var ScriptType = Template['SqlScript'].$['type'];
+        var Command = Template['SqlScript']['Command'];
+        var Parameters = Template['SqlScript']['Parameters'];
+        var ResultType = Template['SqlScript']['ResultType'];
+        var sql = Command[0].toString();
+        var selectSql = '';//select @returnValue, @returnMsg;
 
-            if (par_dir == 'OUT') {
-                if (selectSql == '') selectSql = 'select ';
-                selectSql += par_name + ',';
-            } else {
-                if (typeof (par_value) == 'undefined') {
-                    console.log('Parameter ' + par_name + ' undefined');
-                    r.err = 1;
-                    return r;
+        //if (typeof (Parameters) != 'undefined')
+        if (Parameters.length == 1 && typeof (Parameters[0].par) != 'undefined') {
+            for (var i = 0; i < Parameters[0].par.length; i++) {
+                //https://www.npmjs.com/package/xml2js
+                //attrkey (default: $): Prefix that is used to access the attributes. Version 0.1 default was @.
+                var par = Parameters[0].par[i].$;
+                var par_name = par.name;
+                var par_type = par.type;
+                var par_dir = par.dir;
+                var par_value = postData[par_name.replace('@', '')];
+
+                if (par_dir == 'OUT') {
+                    if (selectSql == '') selectSql = 'select ';
+                    selectSql += par_name + ',';
                 } else {
-                    sql = sql.replace(par_name, mysql.escape(par_value));
+                    if (typeof (par_value) == 'undefined') {
+                        console.log('Parameter ' + par_name + ' undefined');
+                        r.err = 1;
+                        return r;
+                    } else {
+                        sql = sql.replace(par_name, mysql.escape(par_value));
+                    }
                 }
+                //debug('name:%s,value:%s', par.name, par.type);
             }
-            //debug('name:%s,value:%s', par.name, par.type);
         }
-    }
-    r.sql = sql;
-    r.selectSql = selectSql;
 
-    r.ScriptType = ScriptType;
+
+        r.sql = sql;
+        r.selectSql = selectSql;
+
+        r.ScriptType = ScriptType;
+    } catch (err) {
+        r.err = 1;
+        r.err_msg = err;
+    }
     return r;
 
 }
@@ -83,62 +92,75 @@ var LoadTemplet = function (pool, sqlModuleCache, act, postData, callback) {
                     callback(err, '', -2, 'Internal server error,template parse failed..');
 
                 } else {
-                    var r = ParseSqlParameters(data, postData);
+
+
+
+
+                    r = ParseSqlParameters(data, postData);
+
+
 
                     if (r.err > 0) {
                         callback(err, '', -1, 'Script Parameters missing.');
+
+                    } else {
+                        console.log('Script Parameters successfully.');
+                        sql = r.sql;
+                        selectSql = r.selectSql;
+                        ScriptType = r.ScriptType;
+                        pool.getConnection(function (err, connection) {
+                            if (err) {
+                                console.log('Database connection failed');
+                                callback(err, '', -1, 'Database connection failed');
+                                //callback(err, 'Database connection failed');
+                            }
+                            else {
+                                connection.query(sql, function (err, rows, fields) {
+                                    if (err) {
+                                        debug("err:%O,%O,Query data failed：%s", err, rows, sql);
+                                        //callback(err, 'Query data failed.');
+                                        callback(err, '', -2, 'Query data failed.');
+                                    } else {
+                                        debug("Prepare Query [%s] successfully", sql);
+                                        if (ScriptType == 'StoredProcedure') {
+
+                                            selectSql = selectSql.substring(0, selectSql.length - 1) + ';';
+                                            connection.query(selectSql, function (err, return_rows, fields) {
+                                                if (err) {
+                                                    console.log('Query data failed：' + selectSql);
+                                                    //callback(err, 'Query data failed.');
+                                                    callback(err, '', -3, 'Query data failed');
+                                                } else {
+                                                    debug("Prepare Query [%s] successfully", selectSql);
+                                                    //callback(0, rows);
+                                                    var result = rows;
+                                                    var returnValue = return_rows[0]['@returnValue'];
+                                                    var returnMsg = return_rows[0]['@returnMsg'];
+
+                                                    result.PacketCount = result.length;
+                                                    callback(err, result, returnValue, returnMsg);
+                                                }
+                                            });
+
+
+                                            connection.release();
+                                        }
+                                        else {
+
+                                            var result = rows;
+                                            result.PacketCount = result.length;
+                                            callback(err, result, 1, 'QueryOK');
+
+                                        }
+
+                                    }
+                                });
+                                if (ScriptType != 'StoredProcedure')
+                                    connection.release();
+                            }
+
+                        });
                     }
-
-                    sql = r.sql;
-                    selectSql = r.selectSql;
-                    ScriptType = r.ScriptType;
-                    pool.getConnection(function (err, connection) {
-                        if (err) {
-                            console.log('Database connection failed');
-                            callback(err, '', -1, 'Database connection failed');
-                            //callback(err, 'Database connection failed');
-                        }
-                        else {
-                            connection.query(sql, function (err, rows, fields) {
-                                if (err) {
-                                    debug("err:%O,%O,Query data failed：%s", err, rows, sql);
-                                    //callback(err, 'Query data failed.');
-                                    callback(err, '', -2, 'Query data failed.');
-                                } else {
-                                    debug("Prepare Query [%s] successfully", sql);
-                                    if (ScriptType == 'StoredProcedure') {
-
-                                        selectSql = selectSql.substring(0, selectSql.length - 1) + ';';
-                                        connection.query(selectSql, function (err, return_rows, fields) {
-                                            if (err) {
-                                                console.log('Query data failed：' + selectSql);
-                                                //callback(err, 'Query data failed.');
-                                                callback(err, '', -3, 'Query data failed');
-                                            } else {
-                                                debug("Prepare Query [%s] successfully", selectSql);
-                                                //callback(0, rows);
-                                                var result = rows;
-                                                var returnValue = return_rows[0]['@returnValue'];
-                                                var returnMsg = return_rows[0]['@returnMsg'];
-
-                                                result.PacketCount = result.length;
-                                                callback(err, result, returnValue, returnMsg);
-                                            }
-                                        });
-                                    }
-                                    else {
-
-                                        var result = rows;
-                                        result.PacketCount = result.length;
-                                        callback(err, result, 1, 'QueryOK');
-
-                                    }
-
-                                }
-                            });
-                        }
-
-                    });
                 }
             });
         }

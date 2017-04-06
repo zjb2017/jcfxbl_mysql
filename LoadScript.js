21d8e3b3de8c4ad374d409e8228f52507a181af8
xinon <xinon@ZJB.local>
2017-04-06 20:24:19 +0800

    模版文件位置判断采用了同步函数，优先从缓存中加载模版文件。

--- LoadScript.js
+++ LoadScript.js
@@ -60,100 +60,111 @@ var LoadTemplet = function (pool, sqlModuleCache, act, postData, callback) {
     var err_code = 0;
     var err_msg;
 
-    var path = util.format('./sqlModule/user/%s.xml', act);
-    fs.exists(path, function (exists) {
-        if (!exists) {
+    //  var path = util.format('./sqlModule/user/%s.xml', act);
+    // fs.exists(path, function (exists) {
+    //   if (!exists) {
+    //     debug("Internal server error,Template file [%s] not found", path);
+    //   err_msg = 'Internal server error: act ' + act + ' unrecognized';
+    // err_code = 1;
+
+    // callback(1, '', -1, err_msg);
+    //}
+    // else {
+    var moduleFileData;
+    if (typeof (sqlModuleCache[act]) == 'undefined') {
+        //https://nodejs.org/dist/latest-v6.x/docs/api/fs.html
+
+        var path = util.format('./sqlModule/user/%s.xml', act);
+        if (fs.existsSync(path)) {
+            moduleFileData = fs.readFileSync(path);
+            sqlModuleCache[act] = moduleFileData.toString();
+            debug('Read Template file [%s] successfully.', path);
+        } else {
             debug("Internal server error,Template file [%s] not found", path);
             err_msg = 'Internal server error: act ' + act + ' unrecognized';
             err_code = 1;
 
             callback(1, '', -1, err_msg);
+            return;
         }
-        else {
-            var moduleFileData;
-            if (typeof (sqlModuleCache[act]) == 'undefined') {
-                //https://nodejs.org/dist/latest-v6.x/docs/api/fs.html
-                moduleFileData = fs.readFileSync(path);
-                sqlModuleCache[act] = moduleFileData.toString();
-                debug('Read Template file [%s] successfully.', path);
+    } else {
+        moduleFileData = sqlModuleCache[act];
+        debug('Read Template cache [%s] successfully.', act);
+    }
+    //解析xml
+    xmlJS.parseString(moduleFileData, function (err, data) {
+        var sql = '';
+        var selectSql = '';
+        var ScriptType;
+        if (err) {
+            debug('Internal server error,xmlJS.parseString error:%s', data);
+            err_code = 2;
+            err_msg = 'Internal server error,template parse failed.';
+            callback(err, '', -2, 'Internal server error,template parse failed..');
+
+        } else {
+
+            r = ParseSqlParameters(data, postData);
+            if (r.err > 0) {
+                callback(err, '', -1, 'Script Parameters missing.');
+
             } else {
-                moduleFileData = sqlModuleCache[act];
-                debug('Read Template cache [%s] successfully.', act);
-            }
-            //解析xml
-            xmlJS.parseString(moduleFileData, function (err, data) {
-                var sql = '';
-                var selectSql = '';
-                var ScriptType;
-                if (err) {
-                    debug('Internal server error,xmlJS.parseString error:%s', data);
-                    err_code = 2;
-                    err_msg = 'Internal server error,template parse failed.';
-                    callback(err, '', -2, 'Internal server error,template parse failed..');
-
-                } else {
-
-                    r = ParseSqlParameters(data, postData);
-                    if (r.err > 0) {
-                        callback(err, '', -1, 'Script Parameters missing.');
-
-                    } else {
-                        console.log('Script Parameters successfully.');
-                        sql = r.sql;
-                        selectSql = r.selectSql;
-                        ScriptType = r.ScriptType;
-                        pool.getConnection(function (err, connection) {
+                console.log('Script Parameters successfully.');
+                sql = r.sql;
+                selectSql = r.selectSql;
+                ScriptType = r.ScriptType;
+                pool.getConnection(function (err, connection) {
+                    if (err) {
+                        console.log('Database connection failed');
+                        callback(err, '', -1, 'Database connection failed');
+                        //callback(err, 'Database connection failed');
+                    }
+                    else {
+                        connection.query(sql, function (err, rows, fields) {
                             if (err) {
-                                console.log('Database connection failed');
-                                callback(err, '', -1, 'Database connection failed');
-                                //callback(err, 'Database connection failed');
-                            }
-                            else {
-                                connection.query(sql, function (err, rows, fields) {
-                                    if (err) {
-                                        debug("err:%O,%O,Query data failed：%s", err, rows, sql);
-                                        //callback(err, 'Query data failed.');
-                                        callback(err, '', -2, 'Query data failed.');
-                                    } else {
-                                        debug("Prepare Query [%s] successfully", sql);
-                                        if (ScriptType == 'StoredProcedure') {
+                                debug("err:%O,%O,Query data failed：%s", err, rows, sql);
+                                //callback(err, 'Query data failed.');
+                                callback(err, '', -2, 'Query data failed.');
+                            } else {
+                                debug("Prepare Query [%s] successfully", sql);
+                                if (ScriptType == 'StoredProcedure') {
 
-                                            selectSql = selectSql.substring(0, selectSql.length - 1) + ';';
-                                            connection.query(selectSql, function (err, return_rows, fields) {
-                                                if (err) {
-                                                    console.log('Query data failed：' + selectSql);
-                                                    //callback(err, 'Query data failed.');
-                                                    callback(err, '', -3, 'Query data failed');
-                                                } else {
-                                                    debug("Prepare Query [%s] successfully", selectSql);
-                                                    //callback(0, rows);
-                                                    var result = rows;
-                                                    var returnValue = return_rows[0]['@returnValue'];
-                                                    var returnMsg = return_rows[0]['@returnMsg'];
-
-                                                    result.PacketCount = result.length;
-                                                    callback(err, result, returnValue, returnMsg);
-                                                }
-
-                                                connection.release();
-                                            });
-                                        }
-                                        else {
-
+                                    selectSql = selectSql.substring(0, selectSql.length - 1) + ';';
+                                    connection.query(selectSql, function (err, return_rows, fields) {
+                                        if (err) {
+                                            console.log('Query data failed：' + selectSql);
+                                            //callback(err, 'Query data failed.');
+                                            callback(err, '', -3, 'Query data failed');
+                                        } else {
+                                            debug("Prepare Query [%s] successfully", selectSql);
+                                            //callback(0, rows);
                                             var result = rows;
+                                            var returnValue = return_rows[0]['@returnValue'];
+                                            var returnMsg = return_rows[0]['@returnMsg'];
+
                                             result.PacketCount = result.length;
-                                            callback(err, result, 1, 'QueryOK');
-                                            connection.release();
+                                            callback(err, result, returnValue, returnMsg);
                                         }
-                                    }
-                                });
+
+                                        connection.release();
+                                    });
+                                }
+                                else {
+
+                                    var result = rows;
+                                    result.PacketCount = result.length;
+                                    callback(err, result, 1, 'QueryOK');
+                                    connection.release();
+                                }
                             }
                         });
                     }
-                }
-            });
+                });
+            }
         }
     });
+    //}
+    //});
 }
 
 exports.LoadTemplet = LoadTemplet;
